77382e0304f440d7db6118a189a3d9cb0d920691
Tim Stackhouse <tstackhouse@radius180.com>
2014-12-11 14:54:56 -0500

    add git info to the prompt

--- source/30_prompt.sh
+++ source/30_prompt.sh
@@ -1,7 +1,9 @@
 # Ensure we have colors
 autoload -U colors && colors
 
+local git_status='$(git_super_status)'
+
 PROMPT="
 %D{%I:%M %p} [ %{$fg[magenta]%}%~%{$reset_color%} ]
-%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[white]%}%m%{$reset_color%}
+%{$fg[cyan]%}%n%{$reset_color%}@%{$fg[white]%}%m%{$reset_color%} ${git_status}
 %{$fg[green]%}%!%{$reset_color%}$ "
