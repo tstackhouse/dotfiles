659ba4594eb69f387ae917c9825997e2b1a8b856
Tim Stackhouse <tstackhouse@radius180.com>
2018-03-09 18:22:38 -0500

    Try a fix on weasel

--- source/80_ssh_agent.sh
+++ source/80_ssh_agent.sh
@@ -11,20 +11,27 @@ function start_agent {
     /usr/bin/ssh-add;
 }
 
-if [ "${SSH_AUTH_SOCK}" ]; then
+function start_weasel {
+    echo "Connecting to pageant SSH agent via weasel..."
+	eval $(${WEASEL_BIN} -r -a "/tmp/.weasel-pageant-${USER}")
+}
+
+if [ -x "${WEASEL_BIN}" ]; then
+    #ps ${SSH_PAGEANT_PID} doesn't work under cywgin
+	(ps -ef | grep ${SSH_PAGEANT_PID} | grep weasel-pageant) >&| /dev/null || {
+	start_weasel;
+    }
+elif [ "${SSH_AUTH_SOCK}" ]; then
     if [[ -S "$SSH_AUTH_SOCK" && ! -h "$SSH_AUTH_SOCK" ]]; then
         ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock;
     fi
     export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock;
     echo "Using forwarded SSH agent"
-elif [ -x "${WEASEL_BIN}" ]; then
-    echo "Connecting to pageant SSH agent via weasel..."
-	eval $(${WEASEL_BIN} -r -a "/tmp/.weasel-pageant-${USER}")
 elif [ -f "${SSH_ENV}" ]; then
     # Source SSH settings, if applicable
     . "${SSH_ENV}" > /dev/null
     #ps ${SSH_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+    (ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$) >&| /dev/null || {
 	start_agent;
     }
 else
