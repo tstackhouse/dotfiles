9b2bda117b1f9909da2fcb31c149222e7f7d7d5b
Tim Stackhouse <tstackhouse@gmail.com>
2018-09-06 10:44:41 -0400

    zsh completion

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -19,3 +19,6 @@ if is_wsl; then
     export PATH="$PATH:/mnt/c/Program Files/Oracle/VirtualBox"
     export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
 fi
+
+# zsh completion
+fpath=(~/.zsh/completion $fpath)
