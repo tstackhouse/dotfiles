09f5d83389bcc083f8d6b1ac69b8f1d2015d29d2
Tim Stackhouse <tstackhouse@gmail.com>
2020-06-09 12:05:20 -0400

    migrate to wsl2 pageant helper

--- source/80_ssh_agent.sh
+++ source/80_ssh_agent.sh
@@ -1,6 +1,6 @@
 # ssh-agent config
+
 SSH_ENV="$HOME/.ssh-local/environment"
-WEASEL_BIN=/mnt/c/Program\ Files/PuTTY/weasel-pageant
 
 function start_agent {
     echo "Initialising new SSH agent..."
@@ -11,8 +11,13 @@ function start_agent {
     /usr/bin/ssh-add;
 }
 
-if [ -x "${WEASEL_BIN}" ]; then
-    eval $(${WEASEL_BIN} -r -a "/tmp/.weasel-pageant-${USER}")
+if is_wsl; then
+    export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+    ss -a | grep -q $SSH_AUTH_SOCK
+    if [ $? -ne 0 ]; then
+	rm -f $SSH_AUTH_SOCK
+	(setsid nohup socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:$HOME/.ssh/wsl2-ssh-pageant.exe >/dev/null 2>&1 &)
+    fi
 elif [ "${SSH_AUTH_SOCK}" ]; then
     if [[ -S "$SSH_AUTH_SOCK" && ! -h "$SSH_AUTH_SOCK" ]]; then
         ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock;
